209256ae526e2d5e25bb27c8f55bd4366644497c
mao <1296193245@qq.com>
2023-06-19 16:30:50 +0800

    提交-v-text

---
+++ src/App12.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p v-text="text1"></p>
+    <p v-text="text2"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App12",
+  data()
+  {
+    return {
+      text1: "hello",
+      text2: "<h1>你好</h1>",
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>

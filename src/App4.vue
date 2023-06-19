22441c0c92ec6a3f2b3513a469ea4922bec2d06f
mao <1296193245@qq.com>
2023-06-19 14:57:16 +0800

    提交-事件绑定

---
+++ src/App4.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input type="number" :value="number1" v-on:click="m1">
+    <br>
+    <input type="number" :value="number2" @click="m2">
+    <br>
+    <input type="text" :value="name" @change="m3()">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App4",
+  data()
+  {
+    return {
+      number1: 100,
+      number2: 300,
+      name: "张三",
+    }
+  },
+  methods:
+      {
+        m1()
+        {
+          console.log("点击执行m1")
+          this.number1++;
+        },
+        m2()
+        {
+          console.log("点击执行m2")
+          this.number2++;
+        },
+        m3()
+        {
+          console.log("更改事件执行m3")
+          console.log(this.name)
+        }
+      }
+}
+</script>
+
+<style scoped>
+
+</style>

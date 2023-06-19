e81ba263dbdd9ad91abca4a539f66822f2cfa478
mao <1296193245@qq.com>
2023-06-19 14:47:26 +0800

    提交-属性绑定

---
+++ src/App3.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-bind:value="name">
+    <br>
+    <input type="text" :value="address">
+    <br>
+    <input type="number" :value="age">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App3",
+  data()
+  {
+    return {
+      name: "张三",
+      age: 17,
+      address: "中国湖南"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

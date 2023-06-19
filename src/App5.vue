5d1b51d38f86fcd3981e2e4c0c63e7c9f82c1d04
mao <1296193245@qq.com>
2023-06-19 15:08:52 +0800

    提交-双向绑定

---
+++ src/App5.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input type="text" v-model="name" @change="m1">
+    <br>
+    <input type="number" v-model="age" @change="m2">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App5",
+  data()
+  {
+    return {
+      name: "张三",
+      age: 19,
+    }
+  },
+  methods:
+      {
+        m1()
+        {
+          console.log("m1")
+          console.log(this.name)
+        },
+        m2()
+        {
+          console.log("m2")
+          console.log(this.age)
+        }
+      },
+  mounted()
+  {
+    window.setInterval(() =>
+    {
+      this.age++;
+    }, 5000)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

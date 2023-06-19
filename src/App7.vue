52bc4e75777acd652cf49cae2cf34f48e6242bed
mao <1296193245@qq.com>
2023-06-19 15:23:58 +0800

    提交-计算属性

---
+++ src/App7.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h3>{{ fullName }}</h3>
+    <h3>{{ fullName }}</h3>
+    <h3>{{ fullName }}</h3>
+    <h3>{{ fullName }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App6",
+  data()
+  {
+    return {
+      firstName: '三',
+      lastName: '张'
+    }
+  },
+  methods:
+      {
+        /*fullName()
+        {
+          //模拟延时
+          for (let i = 0; i < 100000; i++)
+          {
+            for (let j = 0; j < 30000; j++)
+            {
+
+            }
+          }
+          console.log('进入了 fullName方法')
+          console.log(new Date().getTime())
+          return this.lastName + this.firstName;
+        }*/
+      },
+  computed:
+      {
+        fullName()
+        {
+          //模拟延时
+          for (let i = 0; i < 100000; i++)
+          {
+            for (let j = 0; j < 30000; j++)
+            {
+
+            }
+          }
+          console.log('进入了 fullName方法')
+          console.log(new Date().getTime())
+          return this.lastName + this.firstName;
+        }
+      }
+}
+</script>
+
+<style scoped>
+
+</style>

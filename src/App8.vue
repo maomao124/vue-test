b01fb198424982d4eaabcb94688cb45a3a1a5216
mao <1296193245@qq.com>
2023-06-19 15:38:45 +0800

    提交-条件渲染

---
+++ src/App8.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>
+      是否成年：
+      <strong v-if="age>=18">是</strong>
+      <strong v-else>不是</strong>
+    </p>
+    <p>
+      是否为中国人：
+      <strong v-if="address==='中国'">是</strong>
+      <strong v-else>不是</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App8",
+  data()
+  {
+    return {
+      age: 17,
+      address: "中国"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/App9.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>
+      是否成年：
+      <strong v-show="age>=18">是</strong>
+      <strong v-show="age<18">不是</strong>
+    </p>
+    <p>
+      是否为中国人：
+      <strong v-show="address==='中国'">是</strong>
+      <strong v-show="address!=='中国'">不是</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App9",
+  data()
+  {
+    return {
+      age: 17,
+      address: "中国"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

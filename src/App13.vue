c9cdd6157363cf83f7c6f9bd5d347a739c938af9
mao <1296193245@qq.com>
2023-06-19 16:34:30 +0800

    提交-v-html

---
+++ src/App13.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p v-html="text1"></p>
+    <p v-html="text2"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App13",
+  data()
+  {
+    return {
+      text1: "hello",
+      text2: "<h1>你好</h1>",
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>

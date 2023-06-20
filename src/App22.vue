09c2f1fc96f0569865c2e0ad9b6cd356e85c4b09
mao <1296193245@qq.com>
2023-06-20 21:29:27 +0800

    提交-Vue-Router js代码跳转

---
+++ src/App22.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <el-container>
+    <el-aside width="200px">
+      <div className="link">
+
+        <br>
+        <el-input type="text" size="large" autocomplete="false" v-model="to"/>
+        <br>
+        <el-button type="success" size="large" @click="go">点击跳转</el-button>
+
+      </div>
+    </el-aside>
+    <el-container>
+      <el-header>路由跳转</el-header>
+      <el-main>
+        <div className="all">
+          <router-view className="r"></router-view>
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
+
+</template>
+
+<script>
+export default {
+  name: "App22",
+  data()
+  {
+    return {
+      to: "/",
+    }
+  },
+  methods:
+      {
+        go()
+        {
+          console.log("跳转到：" + this.to)
+          this.$router.push(this.to);
+        }
+      }
+
+}
+</script>
+
+<style scoped>
+.all {
+  padding: 20px;
+  background: #b5e3f1;
+}
+
+.r {
+  background: cornflowerblue;
+}
+
+.link {
+  background: #42b983;
+  height: 100vh;
+}
+</style>

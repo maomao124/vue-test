96d72d133f2e2b7908acd5a4412e76dc90419acc
mao <1296193245@qq.com>
2023-06-25 21:06:13 +0800

    提交-访问模板引用

---
+++ src/App45.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="text" value="test" ref="input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App45",
+
+  mounted()
+  {
+    console.log(this.$refs.input)
+    console.log("value:", this.$refs.input.value)
+    console.log("type:", this.$refs.input.type)
+    this.$refs.input.value = "123456789";
+    console.log("value:", this.$refs.input.value)
+
+    this.$refs.input.focus();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

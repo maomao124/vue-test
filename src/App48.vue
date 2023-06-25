e6554fab901f9c5a8258aa42de75c74c31563a66
mao <1296193245@qq.com>
2023-06-26 00:47:45 +0800

    提交-组件自定义事件-触发与监听事件

---
+++ src/App48.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!--没有注册demo2-->
+    <c1 @demo="f1"></c1>
+    <br>
+    <br>
+    <c1 @demo="f2" @demo2="f3"></c1>
+  </div>
+</template>
+
+<script>
+import {ElMessage} from 'element-plus'
+import c1 from '@/components/C1'
+
+export default {
+  name: "App48",
+  components: {
+    c1: c1,
+  },
+  methods:
+      {
+        f1()
+        {
+          console.log("f1")
+          ElMessage.success({
+            message: "触发自定义demo事件 f1",
+            center: true
+          })
+        },
+        f2()
+        {
+          console.log("f2")
+          ElMessage.success({
+            message: "触发自定义demo事件 f2",
+            center: true
+          })
+        },
+        f3()
+        {
+          console.log("f3")
+          ElMessage.success({
+            message: "触发自定义demo2事件 f3",
+            center: true
+          })
+        }
+      }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/C1.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div class="c1">
+    <p>子组件内容</p>
+    <el-button type="success" @click="send">点击触发自定义事件demo</el-button>
+    <br>
+    <br>
+    <el-button type="success" @click="$emit('demo2')">点击触发自定义事件demo2</el-button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "C1",
+  methods:
+      {
+        send()
+        {
+          this.$emit('demo')
+        }
+      }
+}
+</script>
+
+<style scoped>
+.c1 {
+  width: 200px;
+  height: 200px;
+  background: darksalmon;
+}
+</style>

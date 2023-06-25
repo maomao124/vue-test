c7fe6014a63c5da2d5c7c18a30e08ac347d1030c
mao <1296193245@qq.com>
2023-06-25 21:25:24 +0800

    提交-函数模板引用

---
+++ src/App46.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ui>
+      <li v-for="item of itemList" ref="items">
+        {{ item }}
+      </li>
+    </ui>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App46",
+  data()
+  {
+    return {
+      itemList: ['1', '2', '3', '4', '5']
+    }
+  },
+  mounted()
+  {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/App47.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-model="d" :ref="foo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App47",
+  data()
+  {
+    return {
+      d: "10000",
+    }
+  },
+  methods: {
+    foo(el)
+    {
+      console.log("调用一次 当前value："+el.value)
+      console.log(el)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

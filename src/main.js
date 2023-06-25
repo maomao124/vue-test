57f4835e9364e99251e0e89bae23ab36e7321bc1
mao <1296193245@qq.com>
2023-06-25 13:40:27 +0800

    提交-命令式地创建侦听器

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import App from './App42.vue'
+import App from './App43.vue'
 import Router2 from '@/router/Router11'
 import Store from '@/store/index'
 

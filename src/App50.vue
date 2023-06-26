f427d6b32c4c9807d3e15616e662ac63ecc4916f
mao <1296193245@qq.com>
2023-06-26 22:42:02 +0800

    提交-自定义指令

---
+++ src/App50.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <input type="text" value="200"/>
+  <br>
+  <input type="text" value="100" v-focus/>
+
+</template>
+
+<script>
+export default {
+  name: "App50"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/App51.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 v-background="'#cccccc'">hello</h2>
+    <h2 v-background="'#ffaa00'">hello</h2>
+    <h2 v-background="'#ffaacc'">hello</h2>
+    <h2 v-background="'red'">hello</h2>
+    <h2 v-background="'skyblue'">hello</h2>
+    <h2 v-background="'#00ffaa'">hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App51",
+  directives:
+      {
+        background: {
+          beforeMount: function (el, binding, vnode, prevVnode)
+          {
+            console.log(el, binding.value)
+            el.style.background = binding.value;
+          }
+        }
+      }
+}
+</script>
+
+<style scoped>
+
+</style>

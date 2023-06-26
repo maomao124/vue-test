95fc062a720dd654e3d55149f26d67ab992a1af6
mao <1296193245@qq.com>
2023-06-26 15:34:26 +0800

    提交-组件自定义事件-事件参数

---
+++ src/App49.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <c2 @commit="f1"></c2>
+    <br>
+    <c2 @commit="f2"></c2>
+    <br>
+    <br>
+    <h2>第一个子组件的值：{{data1}}</h2>
+    <h2>第二个子组件的值：{{data2}}</h2>
+  </div>
+</template>
+
+<script>
+import {ElMessage} from 'element-plus'
+import c2 from '@/components/C2'
+
+export default {
+  name: "App49",
+  components: {
+    c2
+  },
+  data()
+  {
+    return {
+      data1: {
+        name: '',
+        address: ''
+      },
+      data2: {
+        name: '',
+        address: ''
+      }
+    }
+  },
+  methods:
+      {
+        f1(name, address)
+        {
+          console.log("f1", name, address)
+          ElMessage.success({
+            message: "触发自定义commit事件f1，值：" + name + " , " + address,
+            center: true
+          })
+          this.data1 = {
+            name: name,
+            address: address
+          }
+        },
+        f2(name, address)
+        {
+          console.log("f2", name, address)
+          ElMessage.success({
+            message: "触发自定义commit事件f2，值：" + name + " , " + address,
+            center: true
+          })
+          this.data2 = {
+            name: name,
+            address: address
+          }
+        },
+      }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/C2.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div class="c2">
+    <p>子组件内容</p>
+    <el-input v-model="name"></el-input>
+    <br>
+    <el-input v-model="address"></el-input>
+    <br>
+    <el-button type="success" @click="send">点击触发自定义事件commit</el-button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "C2",
+  data()
+  {
+    return {
+      name: '',
+      address: ''
+    }
+  },
+  methods:
+      {
+        send()
+        {
+          console.log("子组件向父组件传值：", this.name, this.address)
+          this.$emit('commit', this.name, this.address)
+        }
+      }
+}
+</script>
+
+<style scoped>
+.c2 {
+  width: 400px;
+  height: 200px;
+  background: dodgerblue;
+}
+</style>

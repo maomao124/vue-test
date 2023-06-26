e3707cfa2b45b5904f0cb4fc6c64f387d095e155
mao <1296193245@qq.com>
2023-06-26 23:25:57 +0800

    提交-插件

---
+++ src/App52.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>{{ $translate('a.a1') }}</h2>
+  <h2>{{ $translate('a.a2') }}</h2>
+  <h2>{{ $translate('a.a3') }}</h2>
+  <h2>{{ $translate('b.b1') }}</h2>
+  <h2>{{ $translate('b.b2') }}</h2>
+  <h2>{{ $translate('b.b3') }}</h2>
+
+</template>
+
+<script>
+export default {
+  name: "App52"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,28 @@ import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import App from './App51.vue'
+import App from './App52.vue'
 import Router2 from '@/router/Router11'
 import Store from '@/store/index'
+import i18n from '@/plugins/i18n'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(Router2)
 app.use(Store)
+app.use(i18n, {
+    a:
+        {
+            a1: "hello1",
+            a2: "hello2",
+            a3: "hello3",
+        },
+    b:
+        {
+            b1: "world1",
+            b2: "world2",
+            b3: "world3",
+        }
+})
 app.mount('#app')
